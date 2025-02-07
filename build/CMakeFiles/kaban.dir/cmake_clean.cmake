a6fdbf8e5deea79fba78beba9fac2c15abecec57
amon2526 <i20808753@gmail.com>
2025-02-07 10:19:09 +0100

    Assembled the project structure

---
+++ build/CMakeFiles/kaban.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kaban.dir/src/main.cpp.obj"
+  "CMakeFiles/kaban.dir/src/main.cpp.obj.d"
+  "kaban.exe"
+  "kaban.exe.manifest"
+  "kaban.pdb"
+  "libkaban.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kaban.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/kaban.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kaban.
+# This may be replaced when dependencies are built.
